9cd8cb78fea5225777e138c862d8158164033f8b
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-11-25 15:32:09 +0100

    test

--- hosts/nixos-dev.nix
+++ hosts/nixos-dev.nix
@@ -4,25 +4,14 @@
   ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/ROOT";
-    fsType = "btrfs";
-    options = [ "subvol=nixos" "compress=zstd" "autodefrag" "noatime" ];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/ROOT";
-    fsType = "btrfs";
-    options = [ "subvol=home" "compress=zstd" "autodefrag" "noatime" ];
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/EFI";
+    device = "/dev/disk/by-label/BOOT";
     fsType = "vfat";
   };
-
-  swapDevices = [
-    { device = "/dev/disk/by-label/SWAP"; }
-  ];
 
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [];
@@ -32,7 +21,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.useDHCP = false;
-  networking.interfaces.enp6s18.useDHCP = true;
+  networking.interfaces.enp1s0.useDHCP = true;
 
   services.qemuGuest.enable = true;
 }
