c8e12a196ee1e4179a388aafee035cb3a02d425c
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-11-25 14:05:26 +0100

    add nixos-dev host config

--- hosts/nixos-dev.nix
+++ hosts/nixos-dev.nix
@@ -1,3 +1,24 @@
 { ... }: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
+  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "sr_mod" "virtio_blk" ];
+
+  fileSystems."./" = {
+    device = "/dev/disk/by-partlabel/";
+    fsType = "btrfs";
+    options = [ "subvol=nixos" "compress=zstd" "autodefrag" "noatime" ];
+  };
+
+  fileSystems."./home" = {
+    device = "/dev/disk/by-partlabel/";
+    fsType = "btrfs";
+    options = [ "subvol=home" "compress=zstd" "autodefrag" "noatime" ];
+  };
+
+  fileSystems."./boot" = {
+    device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
+  };
 }
