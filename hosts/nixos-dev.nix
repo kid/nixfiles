837bd424f00dbb58a35db2076f61c15a84e7cf62
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-11-25 14:12:52 +0100

    fix filesystem configuration

--- hosts/nixos-dev.nix
+++ hosts/nixos-dev.nix
@@ -5,19 +5,19 @@
 
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "sr_mod" "virtio_blk" ];
 
-  fileSystems."./" = {
+  fileSystems."/" = {
     device = "/dev/disk/by-partlabel/";
     fsType = "btrfs";
     options = [ "subvol=nixos" "compress=zstd" "autodefrag" "noatime" ];
   };
 
-  fileSystems."./home" = {
+  fileSystems."/home" = {
     device = "/dev/disk/by-partlabel/";
     fsType = "btrfs";
     options = [ "subvol=home" "compress=zstd" "autodefrag" "noatime" ];
   };
 
-  fileSystems."./boot" = {
+  fileSystems."/boot" = {
     device = "/dev/disk/by-label/EFI";
     fsType = "vfat";
   };
