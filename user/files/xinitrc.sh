6c201e2961964d6e386d2a670e5a6d49af5c5d4e
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-12-01 15:46:57 +0100

    fix dbus initialization ion xinitrc

--- user/files/xinitrc.sh
+++ user/files/xinitrc.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env sh
 
-if [ "$(xrandr --listmonitors | head -n1 | cut -d ' ' -f 2)" != "1" ]; then
-  xrandr --output DP-4 --scale 1x1 --mode 3840x1600 --rate 119.98 --pos 0x480 --primary
-  xrandr --output DP-2 --scale 1x1 --mode 2560x1440 --rate 119.98 --pos 3840x0 --rotate left
+if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+	eval "$(dbus-launch --exit-with-session --sh-syntax)"
 fi
 
-exec dbus-launch xmonad-kid
+systemctl --user import-environment DISPLAY XAUTHORITY
+
+if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+	dbus-update-activation-environment DISPLAY XAUTHORITY
+fi
+
+if [ "$(xrandr --listmonitors | head -n1 | cut -d ' ' -f 2)" != "1" ]; then
+	xrandr --output DP-4 --scale 1x1 --mode 3840x1600 --rate 119.98 --pos 0x480 --primary
+	xrandr --output DP-2 --scale 1x1 --mode 2560x1440 --rate 119.98 --pos 3840x0 --rotate left
+fi
+
+exec xmonad-kid
