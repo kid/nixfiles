45ae00caf2c6e1436b6a53a7e98220ca19f5b5aa
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-12-24 13:12:17 +0100

    use 144hz

--- home/files/xinitrc.sh
+++ home/files/xinitrc.sh
@@ -11,8 +11,8 @@ if command -v dbus-update-activation-environment >/dev/null 2>&1; then
 fi
 
 if [ "$(xrandr --listmonitors | head -n1 | cut -d ' ' -f 2)" != "1" ]; then
-	xrandr --output DP-4 --scale 1x1 --mode 3840x1600 --rate 119.98 --pos 0x480 --primary
-	xrandr --output DP-2 --scale 1x1 --mode 2560x1440 --rate 119.98 --pos 3840x0 --rotate left
+	xrandr --output DP-4 --scale 1x1 --mode 3840x1600 --rate 144 --pos 0x480 --primary
+	xrandr --output DP-2 --scale 1x1 --mode 2560x1440 --rate 144 --pos 3840x0 --rotate left
 fi
 
 exec xmonad-kid
