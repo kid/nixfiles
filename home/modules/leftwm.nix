922c46ad1e11199864c8bb03266952e34211dab5
Arnaud Rebts <arnaud.rebts@gmail.com>
2021-12-31 18:39:05 +0100

    add leftwm theme

--- home/modules/leftwm.nix
+++ home/modules/leftwm.nix
@@ -1,3 +1,9 @@
 {
   home.file.".config/leftwm/config.toml".source = ../files/leftwm/config.toml;
+  home.file.".config/leftwm/themes/current/up".source = ../files/leftwm/up;
+  home.file.".config/leftwm/themes/current/down".source = ../files/leftwm/down;
+  home.file.".config/leftwm/themes/current/change_to_tag".source = ../files/leftwm/change_to_tag;
+  home.file.".config/leftwm/themes/current/theme.toml".source = ../files/leftwm/theme.toml;
+  home.file.".config/leftwm/themes/current/polybar.ini".source = ../files/leftwm/polybar.ini;
+  home.file.".config/leftwm/themes/current/template.liquid".source = ../files/leftwm/template.liquid;
 }
