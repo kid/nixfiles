066a9866a3dd16f286e4c80169dd4f588040c8dd
Arnaud Rebts <arnaud.rebts@gmail.com>
2022-05-26 13:25:37 +0200

    add some git aliases

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -7,7 +7,15 @@
     userEmail = "arnaud.rebts@gmail.com";
 
     aliases = {
+      br = "branch";
+      ci = "commit";
+      cl = "clone";
       co = "checkout";
+      cp = "cherry-pick";
+      ls = "log --decorate --oneline";
+      ll = "log --decorate --numstat";
+      lg = "log --decorate --graph --abbrev-commit --date=relative --all";
+      st = "status -s";
     };
   };
 }
