0bb55c15d6de3abb943e07f4530e48502aeee8b4
Arnaud Rebts <arnaud.rebts@gmail.com>
2022-07-10 08:02:13 +0200

    feat(git): replace delta with difftastic
    Add extra git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,7 +2,7 @@
 {
   programs.git = {
     enable = true;
-    delta.enable = true;
+    difftastic.enable = true;
     userEmail = "arnaud.rebts@gmail.com";
     userName = "Arnaud Rebts";
 
@@ -16,6 +16,34 @@
       ll = "log --decorate --numstat";
       lg = "log --decorate --graph --abbrev-commit --date=relative --all";
       st = "status -s";
+    };
+
+    extraConfig = {
+      fetch = {
+        prune = true;
+      };
+      push = {
+        default = "simple";
+        followTags = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      merge = {
+        ff = "only";
+      };
+      mergetool = {
+        keepBackup = false;
+      };
+      rebase = {
+        autosquash = true;
+      };
+      rerere = {
+        enabled = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
     };
   };
 }
