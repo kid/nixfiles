186c27aac2b327b94de1f7ffdfc328c8b431b93b
Arnaud Rebts <arnaud.rebts@gmail.com>
2022-08-10 14:13:34 +0200

    ssh: enable compression and agent forwarding

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -2,7 +2,9 @@
   # Client side SSH configuration
   programs.ssh = {
     enable = true;
+    compression = true;
     controlMaster = "auto";
     controlPersist = "10m";
+    forwardAgent = true;
   };
 }
