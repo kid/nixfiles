4b8d60a51b88348c22763f8252f1895f270f7924
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-04-05 00:23:51 +0200

    add user to incus-admin group

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -12,7 +12,7 @@
         isNormalUser = true;
         createHome = true;
         useDefaultShell = true;
-        extraGroups = [ "audio" "video" "wheel" "libvirtd" "docker" ];
+        extraGroups = [ "audio" "video" "wheel" "libvirtd" "incus-admin" "docker" ];
         initialPassword = "foo";
       };
     };
