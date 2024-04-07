c976087358e17889b3de6a572da40eea21bfdb0c
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-04-07 17:43:25 +0200

    cleanup inputs

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,35 +1,32 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   programs.gamescope = {
     enable = true;
     capSysNice = false;
-    package = pkgs.gamescope_git;
-    
+    # package = pkgs.gamescope_git;
+
   };
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
     package = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
-      ];
+      extraPkgs = pkgs:
+        with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
     };
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
   # chaotic.steam.extraCompatPackages = with pkgs; [
   # ];
-
 
   programs.noisetorch.enable = true;
 
