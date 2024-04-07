63172eaba0d18f9961103d7c832773e6f73a83fc
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-04-07 17:13:12 +0200

    Cleanup inputs, add a separate plasma6 profile

---
+++ profiles/plasma6.nix
@@ -0,0 +1,46 @@
+{ inputs, pkgs, ... }: {
+  imports = [
+    # ../modules/nixos/wayland
+    ../modules/nixos/wine.nix
+    ../modules/nixos/sddm.nix
+  ];
+
+  hm.imports = [
+    # inputs.hyprland.homeManagerModules.default
+    ../modules/home-manager/desktop.nix
+    # ../modules/home-manager/leftwm.nix
+    # ../modules/home-manager/wayland
+    # ../modules/home-manager/wayland/hyprland
+    ./home-manager/desktop.nix
+  ];
+
+  user.name = "kid";
+
+  # programs.hyprland = {
+  #   enable = true;
+  #   package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  #   xwayland.enable = true;
+  # };
+  #
+  # services.xserver = {
+  #   enable = true;
+  #   displayManager.gdm = {
+  #     enable = true;
+  #     wayland = true;
+  #   };
+  # };
+
+  # xdg.portal.enable = true;
+  # xdg.portal.wlr.enable
+  # xdg.portal = {
+  #   enable = true;
+  #   wlr.enable = true;
+  #   extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
+  # };
+  # xdg.portal.extraPortals = [ pkgs.xdg-desktop-gtk ];
+
+  # Use wayland where possible (electron)
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # environment.systemPackages = with pkgs; [ hyprland wayland wdisplays ];
+}
