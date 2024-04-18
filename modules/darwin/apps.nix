6d03c59a2b7a3bf02a48ba0c030817b0a0023106
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-04-18 15:46:48 +0200

    darwin: add postgresql and pgAdmin4

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -2,8 +2,8 @@
   homebrew = {
     taps = [ "jakehilborn/jakehilborn" ];
     brews = [ "displayplacer" "webp" ];
-    casks = [ "1password-beta" "scroll-reverser" "spotify" ];
+    casks = [ "1password-beta" "scroll-reverser" "spotify" "pgAdmin4" ];
   };
 
-  hm.home.packages = with pkgs; [ colima docker ];
+  hm.home.packages = with pkgs; [ colima docker postgresql ];
 }
