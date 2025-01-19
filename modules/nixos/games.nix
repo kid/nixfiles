79d178bd8e3115ccdefd07d14d2464b8614f6928
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-01-19 11:09:40 +0100

    fix & tweaks for gaming

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,61 +1,74 @@
 { pkgs, ... }:
 {
-  programs.gamescope = {
-    enable = true;
-    # capSysNice = false;
-    # package = pkgs.gamescope_git;
-  };
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-    package = pkgs.steam.override {
-      extraPkgs =
-        pkgs: with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-        ];
+  programs = {
+    gamemode = {
+      enable = true;
+      enableRenice = true;
+      settings = {
+        general = {
+          renice = 20;
+        };
+      };
     };
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    gamescope = {
+      enable = true;
+      # capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      package = pkgs.steam.override {
+        extraLibraries = pkgs: [ pkgs.xorg.libxcb ];
+        extraPkgs =
+          pkgs: with pkgs; [
+            # gamemode
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
+      };
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    };
+
+    noisetorch.enable = true;
   };
-  # chaotic.steam.extraCompatPackages = with pkgs; [
-  # ];
 
-  programs.noisetorch.enable = true;
+  # hardware.xpadneo.enable = true;
 
-  hardware.xpadneo.enable = true;
+  environment = {
+    sessionVariables.MANGOHUD_CONFIG = "~/.config/MangoHud/MangoHud.conf";
+    systemPackages = with pkgs; [
+      # steamtinkerlaunch
+      # gamemode
+      # heroic
+      # legendary-gl
+      # lutris
 
-  environment.systemPackages = with pkgs; [
-    # steamtinkerlaunch
-    gamemode
-    # heroic
-    # legendary-gl
-    # lutris
+      # wine-staging (version with experimental features)
+      # wineWowPackages.staging
 
-    # wine-staging (version with experimental features)
-    # wineWowPackages.staging
+      # winetricks and other programs depending on wine need to use the same wine version
+      # (winetricks.override { wine = wineWowPackages.staging; })
 
-    # winetricks and other programs depending on wine need to use the same wine version
-    # (winetricks.override { wine = wineWowPackages.staging; })
+      # vulkan-tools
+      # vulkan-loader
+      # vulkan-validation-layers
+      # vulkan-hdr-layer
 
-    # vulkan-tools
-    # vulkan-loader
-    # vulkan-validation-layers
-    # vulkan-hdr-layer
+      # xorg.xrandr
 
-    # xorg.xrandr
-
-    # ckan # mod manager for ksp
-    # steam-run
-    # protontricks
-    mangohud
-    dxvk
-  ];
+      # ckan # mod manager for ksp
+      # steam-run
+      # protontricks
+      mangohud
+      dxvk
+    ];
+  };
 }
