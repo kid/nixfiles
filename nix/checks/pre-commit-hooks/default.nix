7fc54baca4bb9f2cbf51fdbd7fff810d48d74f78
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-02-09 14:33:40 +0100

    refactor(snowfall): introduce snowfall module hierarchy

---
+++ nix/checks/pre-commit-hooks/default.nix
@@ -0,0 +1,41 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  inherit (inputs) git-hooks-nix;
+in
+git-hooks-nix.lib.${pkgs.system}.run {
+  src = ./.;
+  hooks = {
+    nixfmt-rfc-style = {
+      enable = true;
+    };
+
+    deadnix = {
+      enable = true;
+      settings = {
+        edit = true;
+      };
+    };
+
+    git-cliff = {
+      enable = false;
+    };
+
+    shfmt.enable = true;
+
+    statix.enable = true;
+
+    # pre-commit-hook-ensure-sops-enable = true;
+    treefmt = {
+      enable = false;
+      # settings.fail-on-change = true;
+      # programs = {
+      #   nixfmt.enable = true;
+      #   nixfmt.package = pkgs.nixfmt-rfc-style;
+      # };
+    };
+  };
+}
---
+++ nix/modules/nixos/theme/stylix/default.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  namespace,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.${namespace}) mkModule;
+in
+mkModule ./. config { } (_: {
+  stylix = {
+    enable = true;
+    image = ./gruvbox-dark-rainbow.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    fonts = {
+      monospace = {
+        name = "JetBrainsMono Nerd Font";
+        package = pkgs.nerd-fonts.jetbrains-mono;
+      };
+      sizes.terminal = lib.mkDefault 10;
+    };
+    polarity = "dark";
+  };
+})
