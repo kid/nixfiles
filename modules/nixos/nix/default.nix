cf2fe48e7fd4ea8a3d7cdd163fca903859dc5bf4
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-02-28 14:36:33 +0100

    feat: add our own cache

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -58,6 +58,7 @@ mkModule ./. false config
 
           substituters = [
             "https://cache.nixos.org"
+            "https://kidibox.cachix.org"
             "https://nix-community.cachix.org"
             "https://hyprland.cachix.org"
             "https://devenv.cachix.org"
@@ -66,6 +67,7 @@ mkModule ./. false config
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "kidibox.cachix.org-1:BN875x9JUW61souPxjf7eA5Uh2k3A1OSA1JIb/axGGE="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
             "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
