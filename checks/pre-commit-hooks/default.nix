e941b581e26c137e0f954e942bc5cef840fa346d
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-03-02 16:27:48 +0100

    ci: use treefmt for all checks

--- checks/pre-commit-hooks/default.nix
+++ checks/pre-commit-hooks/default.nix
@@ -9,28 +9,9 @@ in
 git-hooks-nix.lib.${pkgs.system}.run {
   src = ./.;
   hooks = {
-    # nixfmt-rfc-style = {
-    #   enable = true;
-    # };
-
-    deadnix = {
-      enable = true;
-      # settings = {
-      #   edit = true;
-      # };
-    };
-
-    # git-cliff = {
-    #   enable = false;
-    # };
-
-    shfmt.enable = true;
-
-    statix.enable = true;
-
     treefmt = {
-      enable = false;
-      packageOverrides.treefmt = inputs.treefmt-nix.lib.mkWrapper pkgs ../../../treefmt.nix;
+      enable = true;
+      packageOverrides.treefmt = inputs.treefmt-nix.lib.mkWrapper pkgs ../../treefmt.nix;
     };
   };
 }
--- treefmt.nix
+++ treefmt.nix
@@ -2,12 +2,28 @@
   projectRootFile = "flake.nix";
 
   programs = {
-    nixfmt.enable = true;
-    just.enable = true;
+    deadnix.enable = true;
     statix.enable = true;
-    shfmt = {
-      enable = true;
-      indent_size = 4;
+    nixfmt.enable = true;
+    prettier.enable = true;
+    just.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
+    # Force use of editorconfig
+    shfmt.indent_size = null;
+  };
+
+  settings = {
+    global.excludes = [
+      "LICENSE"
+      # unsupported extensions
+      "*.{gif,png,svg,tape,mts,lock,mod,sum,toml,env,envrc,org,yuck,gitignore,editorconfig}"
+    ];
+
+    formatter = {
+      deadnix.priority = 1;
+      statix.priority = 2;
+      nixfmt.priotity = 3;
     };
   };
 }
