89bbf4dc3910dfef10082d767f64765d347e4f46
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-03-07 20:48:09 +0100

    feat: add new server module

--- modules/nixos/archetypes/server/default.nix
+++ modules/nixos/archetypes/server/default.nix
@@ -11,20 +11,7 @@ mkModule ./. false config { } (_cfg: {
   ${namespace} = {
     suites = {
       common = enabled;
-    };
-    hardware = {
-      firmware.enable = true;
-      power = {
-        governor = "powersave";
-        energy_performance_preference = "balance_power";
-      };
-    };
-    system = {
-      boot = {
-        enable = true;
-        silent = false;
-        plymouth = false;
-      };
+      server = enabled;
     };
   };
 })
