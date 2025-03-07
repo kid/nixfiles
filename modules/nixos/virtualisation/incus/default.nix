193994a2c32e245eaf63e33a7fcd57afc9cf6e9b
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-03-07 20:49:57 +0100

    feat(incus): add virtualisation.incus module

---
+++ modules/nixos/virtualisation/incus/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  inherit (lib.${namespace}) mkModule;
+in
+mkModule ./. false config { } (_: {
+  virtualisation.incus = {
+    enable = true;
+    ui.enable = true;
+    preseed = {
+      config."core.https_address" = "[::]:8443";
+      networks = [ ];
+      profiles = [ ];
+      storage_pools = [
+        {
+          config = {
+            source = "/var/lib/incus/storage-pools/default";
+          };
+          driver = "dir";
+          name = "default";
+        }
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ incus ];
+
+  networking = {
+    nftables.enable = true;
+
+    useDHCP = false;
+
+    bridges."br0".interfaces = [ "enp36s0f0" ];
+    interfaces.br0.useDHCP = true;
+  };
+
+  users.users.${config.${namespace}.user.name}.extraGroups = [ "incus-admin" ];
+})
