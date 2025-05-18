255ddc88ab29b42f574b635a210cfdae5a9161c3
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-05-18 13:22:38 +0200

    chore: remove commented code

--- homes/kid@nixos/default.nix
+++ homes/kid@nixos/default.nix
@@ -4,7 +4,6 @@
     "${self}/legacy/modules/home-manager"
     "${self}/legacy/modules/home-manager/desktop.nix"
     "${self}/legacy/modules/home-manager/plasma.nix"
-    # "${self}/legacy/modules/home-manager/xremap.nix"
   ];
 
   nixfiles = {
