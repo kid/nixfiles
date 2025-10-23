954afc7ecfc446ae50baf6a89763cb908e00e9b6
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-10-23 22:28:40 +0200

    fix: broken git config

--- modules/home/programs/cli/git.nix
+++ modules/home/programs/cli/git.nix
@@ -8,7 +8,38 @@
       user = {
         email = "arnaud.rebts@gmail.com";
         name = "Arnaud Rebts";
+        signingkey = "~/.ssh/id_ed25519.pub";
       };
+
+      fetch = {
+        prune = true;
+      };
+      push = {
+        default = "simple";
+        followTags = true;
+        autoSetupRemote = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      merge = {
+        ff = "only";
+      };
+      mergetool = {
+        keepBackup = false;
+      };
+      rebase = {
+        autosquash = true;
+      };
+      rerere = {
+        enabled = true;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      tag.gpgsign = true;
 
       aliases = {
         co = "checkout";
@@ -21,39 +52,6 @@
         lg = "log --decorate --graph --abbrev-commit --date=relative --all";
         st = "status -s";
         diffn = "diff --no-ext-diff";
-      };
-
-      extraConfig = {
-        fetch = {
-          prune = true;
-        };
-        push = {
-          default = "simple";
-          followTags = true;
-          autoSetupRemote = true;
-        };
-        pull = {
-          rebase = true;
-        };
-        merge = {
-          ff = "only";
-        };
-        mergetool = {
-          keepBackup = false;
-        };
-        rebase = {
-          autosquash = true;
-        };
-        rerere = {
-          enabled = true;
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        user.signingkey = "~/.ssh/id_ed25519.pub";
-        gpg.format = "ssh";
-        commit.gpgsign = true;
-        tag.gpgsign = true;
       };
     };
   };
