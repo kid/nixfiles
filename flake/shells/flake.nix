490dd19ecb2b7dbd6b6f0bb3364c3423a643d2f3
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-12-01 11:52:27 +0100

    feat: add basic support for Darwin

--- flake/shells/flake.nix
+++ flake/shells/flake.nix
@@ -5,28 +5,14 @@
 }:
 {
   packages = with pkgs; [
-    fd
     just
-    nil
 
     sops
     act
 
+    nil
+    nixd
     deadnix
-    nix-inspect
-    nix-bisect
-    nix-diff
-    nix-health
-    nix-index
-    nix-melt
-    nix-output-monitor
-    nix-prefetch-git
-    nix-search-cli
-    nix-tree
-    nixpkgs-hammering
-    nixpkgs-lint
-    nh
-    nvd
     statix
 
     # inputs.self.checks.${system}.pre-commit-hooks.enabledPackages
