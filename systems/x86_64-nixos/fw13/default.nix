d3f11cf1efba7be615764ea83cb7285196417355
Arnaud Rebts <arnaud.rebts@gmail.com>
2026-03-15 11:09:46 +0100

    fix(fw13): use wpa_supplicant instead of iwd

--- systems/x86_64-nixos/fw13/default.nix
+++ systems/x86_64-nixos/fw13/default.nix
@@ -4,27 +4,27 @@
   inputs,
   ...
 }:
-let
-  # MT7925 WiFi driver patches - fixes kernel panics, mutex deadlocks, and system hangs
-  # https://github.com/zbowling/mt7925
-  mt7925PatchDir = "${inputs.mt7925}/kernels/6.18";
-
-  mt7925Patches =
-    let
-      patchFiles = lib.sort (a: b: a < b) (lib.attrNames (builtins.readDir mt7925PatchDir));
-      mt7925PatchFiles = lib.filter (file: lib.hasSuffix ".patch" file) patchFiles;
-    in
-    map (file: {
-      name = lib.removeSuffix ".patch" file;
-      patch = "${mt7925PatchDir}/${file}";
-    }) mt7925PatchFiles;
-in
+# let
+#   # MT7925 WiFi driver patches - fixes kernel panics, mutex deadlocks, and system hangs
+#   # https://github.com/zbowling/mt7925
+#   mt7925PatchDir = "${inputs.mt7925}/kernels/6.18";
+#
+#   mt7925Patches =
+#     let
+#       patchFiles = lib.sort (a: b: a < b) (lib.attrNames (builtins.readDir mt7925PatchDir));
+#       mt7925PatchFiles = lib.filter (file: lib.hasSuffix ".patch" file) patchFiles;
+#     in
+#     map (file: {
+#       name = lib.removeSuffix ".patch" file;
+#       patch = "${mt7925PatchDir}/${file}";
+#     }) mt7925PatchFiles;
+# in
 {
   imports = with inputs.nixos-hardware.nixosModules; [
     framework-amd-ai-300-series
   ];
 
-  boot.kernelPatches = mt7925Patches;
+  # boot.kernelPatches = mt7925Patches;
 
   nixfiles = {
     device.profiles = [
@@ -64,11 +64,11 @@ in
   networking = {
     useNetworkd = lib.mkForce false;
     networkmanager.enable = lib.mkForce true;
-    networkmanager.wifi.backend = "iwd";
-    wireless.iwd.enable = true;
+    wireless.enable = true;
+    wireless.userControlled = true;
   };
 
-  powerManagement.powertop.enable = true;
+  # powerManagement.powertop.enable = true;
 
   hardware = {
     # FIXME: requirement for xremap, move it there
