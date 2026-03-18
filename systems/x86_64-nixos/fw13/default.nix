96f172f43303fa125e52f4a4265899db70ba1a77
Arnaud Rebts <arnaud.rebts@gmail.com>
2026-03-18 18:57:37 +0100

    fix(fw13): remove kernel patches for mt7925 as I switched to interl card

--- systems/x86_64-nixos/fw13/default.nix
+++ systems/x86_64-nixos/fw13/default.nix
@@ -4,27 +4,10 @@
   inputs,
   ...
 }:
-# let
-#   # MT7925 WiFi driver patches - fixes kernel panics, mutex deadlocks, and system hangs
-#   # https://github.com/zbowling/mt7925
-#   mt7925PatchDir = "${inputs.mt7925}/kernels/6.18";
-#
-#   mt7925Patches =
-#     let
-#       patchFiles = lib.sort (a: b: a < b) (lib.attrNames (builtins.readDir mt7925PatchDir));
-#       mt7925PatchFiles = lib.filter (file: lib.hasSuffix ".patch" file) patchFiles;
-#     in
-#     map (file: {
-#       name = lib.removeSuffix ".patch" file;
-#       patch = "${mt7925PatchDir}/${file}";
-#     }) mt7925PatchFiles;
-# in
 {
   imports = with inputs.nixos-hardware.nixosModules; [
     framework-amd-ai-300-series
   ];
-
-  # boot.kernelPatches = mt7925Patches;
 
   nixfiles = {
     device.profiles = [
