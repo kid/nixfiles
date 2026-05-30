ddcb80555a2f6cd659afd5031f3f2d309b538f7f
Arnaud Rebts <arnaud.rebts@gmail.com>
2026-05-30 11:32:35 +0200

    refactor: gaming aspect (#120)
    * refactor: gaming aspect
    
    * refactor: move xdg configuration to separate nf.desktop
    
    * fix: deprecation in ssh configuration

---
+++ modules/nixfiles/desktop/gaming.nix
@@ -0,0 +1,93 @@
+{
+  inputs,
+  den,
+  nf,
+  ...
+}:
+{
+  nf.desktop.gaming = {
+    nixos =
+      { pkgs, ... }:
+      {
+        nix.settings = {
+          extra-substituters = [ "https://nix-cache.tokidoki.dev/tokidoki" ];
+          extra-trusted-public-keys = [ "tokidoki:MD4VWt3kK8Fmz3jkiGoNRJIW31/QAm7l1Dcgz2Xa4hk=" ];
+        };
+
+        nixpkgs.overlays = [ inputs.nix-gaming-edge.overlays.default ];
+
+        programs = {
+          steam = {
+            enable = true;
+
+            extraCompatPackages = with pkgs; [
+              proton-ge-bin
+              proton-cachyos
+            ];
+
+            package = pkgs.steam.override {
+              extraPkgs =
+                pkgs': with pkgs'; [
+                  mangohud
+                  gamemode
+                  libxcursor
+                  libxi
+                  libxinerama
+                  libxscrnsaver
+                  libpng
+                  libpulseaudio
+                  libvorbis
+                  stdenv.cc.cc.lib
+                  libkrb5
+                  keyutils
+                ];
+            };
+
+            platformOptimizations.enable = true;
+          };
+
+          gamemode = {
+            enable = true;
+            enableRenice = true;
+            settings = {
+              general = {
+                softrealtime = "auto";
+                renice = 10;
+              };
+              custom = {
+                start = "${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations activated'";
+                end = "${pkgs.libnotify}/bin/notify-send -a 'Gamemode' 'Optimizations deactivated'";
+              };
+            };
+          };
+
+          wine = {
+            enable = true;
+            ntsync = true;
+          };
+        };
+
+        environment.sessionVariables = {
+          MESA_VK_WSI_PRESENT_MODE = "immediate";
+          PROTON_USE_NTSYNC = 1;
+        };
+      };
+
+    includes = [
+      (den.lib.policy.when ({ host, ... }: host.hasAspect nf.desktop.wayland) {
+        nixos =
+          { lib, ... }:
+          {
+            programs.gamescope.args = lib.mkAfter [ "--expose-wayland" ];
+          };
+
+        homeManager = _: {
+          home.sessionVariables = {
+            PROTON_ENABLE_WAYLAND = "1";
+            PROTON_ENABLE_HDR = "1";
+          };
+        };
+      })
+    ];
+  };
+}
--- modules/users/kid.nix
+++ modules/users/kid.nix
@@ -14,25 +14,13 @@
       nf.apps._
       nf.shell.zsh
       # nf.ai.pi
+      nf.desktop
       nf.desktop.xremap
-      nf.desktop.xremap.kde
       nf.desktop.plasma
-      (den.provides.unfree [
-        "1password"
-        "1password-cli"
-        "discord"
-        "improved-tube"
-        "onepassword-password-manager"
-        "steam"
-        "steam-unwrapped"
-        "xone-dongle-firmware"
-        "winbox"
-      ])
     ];
 
     homeManager =
       {
-        config,
         pkgs,
         osConfig,
         ...
@@ -75,7 +63,6 @@
         systemd.user.startServices = lib.mkDefault "sd-switch";
 
         programs = {
-
           htop.enable = true;
           btop.enable = true;
           bottom.enable = true;
@@ -87,9 +74,9 @@
           ssh = {
             enable = true;
             enableDefaultConfig = false;
-            matchBlocks."*" = {
-              compression = true;
-              forwardAgent = true;
+            settings."*" = {
+              Compression = true;
+              ForwardAgent = true;
             };
           };
 
@@ -102,55 +89,10 @@
               "ctrl+shift+l=goto_split:right"
             ];
           };
-
-          zed-editor = {
-            enable = true;
-            userSettings.vim_mode = true;
-          };
         };
 
-        xdg = {
-          enable = true;
-          mimeApps = {
-            enable = true;
-            associations.added = {
-              "application/json" = [ "nvim.desktop" ];
-              "text/english" = [ "nvim.desktop" ];
-              "text/plain" = [ "nvim.desktop" ];
-              "text/x-makefile" = [ "nvim.desktop" ];
-              "text/x-c++hdr" = [ "nvim.desktop" ];
-              "text/x-c++src" = [ "nvim.desktop" ];
-              "text/x-chdr" = [ "nvim.desktop" ];
-              "text/x-csrc" = [ "nvim.desktop" ];
-              "text/x-java" = [ "nvim.desktop" ];
-              "text/x-moc" = [ "nvim.desktop" ];
-              "text/x-pascal" = [ "nvim.desktop" ];
-              "text/x-tcl" = [ "nvim.desktop" ];
-              "text/x-tex" = [ "nvim.desktop" ];
-              "application/x-shellscript" = [ "nvim.desktop" ];
-              "text/x-c" = [ "nvim.desktop" ];
-              "text/x-c++" = [ "nvim.desktop" ];
-              "video/*" = [ "mpv.desktop" ];
-              "audio/*" = [ "mpv.desktop" ];
-              "text/html" = [ "firefox.desktop" ];
-              "x-scheme-handler/http" = [ "firefox.desktop" ];
-              "x-scheme-handler/https" = [ "firefox.desktop" ];
-              "x-scheme-handler/ftp" = [ "firefox.desktop" ];
-              "x-scheme-handler/about" = [ "firefox.desktop" ];
-              "x-scheme-handler/unknown" = [ "firefox.desktop" ];
-              "x-scheme-handler/discord" = [ "Discord.desktop" ];
-            };
-            defaultApplications = config.xdg.mimeApps.associations.added;
-          };
-          autostart = {
-            enable = true;
-            entries = [
-              "${pkgs._1password-gui}/share/applications/1password.desktop"
-              "${pkgs.telegram-desktop}/share/applications/org.telegram.desktop.desktop"
-            ];
-          };
-        };
       };
+
     provides = {
       fw13.homeManager = {
         programs.plasma.input.touchpads = [
